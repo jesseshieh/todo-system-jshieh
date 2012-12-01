5c113a6284cd4e80cbad113b1563e6f048375c1a
jesse.shieh <jesse.shieh@bf4c7812-c420-712a-63db-73638799ff6d>
2012-12-01 19:13:07 +0000

    skip empty tasks

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import logging
 import cgi
 import datetime
@@ -40,18 +24,6 @@ decorator = OAuth2Decorator(client_id=settings.CLIENT_ID,
                             scope=settings.SCOPE,
                             user_agent='todo-system-jshieh')
 
-class Context(db.Model):
-  creation_time = db.DateTimeProperty(auto_now_add=True)
-  last_modified_time = db.DateTimeProperty(auto_now=True)
-  name = db.StringProperty(required=True)
-
-class Task(db.Model):
-  creation_time = db.DateTimeProperty(auto_now_add=True)
-  last_modified_time = db.DateTimeProperty(auto_now=True)
-  context = db.ReferenceProperty(db.Model, required=False)
-  name = db.StringProperty(required=True)
-  complete = db.BooleanProperty(default=False)
-
 class MainHandler(webapp2.RequestHandler):
 	
   @decorator.oauth_required
@@ -66,11 +38,16 @@ class MainHandler(webapp2.RequestHandler):
 
     result = service.tasks().list(tasklist=context['id'], showCompleted=False, showDeleted=False, showHidden=False).execute()
     tasks = result.get('items', [])
-    logging.error(tasks)
+
     task = {}
     if tasks:
       # tasks gets added to the front of the list.  grab the last one.
-      task = tasks[-1]
+      # skip empty tasks
+      for t in reversed(tasks):
+        if t['title']:
+          task = t
+          break
+
 
     template_values = {
       'task': task,
